dbcf7bd54a584457421a9b6617403f18cd4a04bf
IRFAN-1920 <irfansiddik1304@gmail.com>
2026-02-10 14:32:55 +0530

    update index and package

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+// Home route
 app.get("/", (req, res) => {
   res.send("🥨 Habeebi Snacks Backend Running");
 });
 
+// Products API
 app.get("/api/products", (req, res) => {
   res.json([
     { id: 1, name: "Mixture", price: 240 },
@@ -19,19 +21,23 @@ app.get("/api/products", (req, res) => {
     { id: 4, name: "Rava Laddu", price: 5 },
     { id: 5, name: "Pooranam", price: 15 },
     { id: 6, name: "Arisivadagam", price: 140 },
-    { id: 7, name: "Penium", price: 10 }
+    { id: 7, name: "Penium", price: 10 },
     { id: 8, name: "Murukku", price: 5 }
-
   ]);
 });
 
+// Order API
 app.post("/api/order", (req, res) => {
+  const order = req.body;
+
   res.json({
     message: "Order placed successfully",
-    orderId: "HB" + Date.now()
+    orderId: "HB" + Date.now(),
+    order
   });
 });
 
+// Server start
 app.listen(PORT, () => {
   console.log(`🚀 Habeebi Snacks Server running on port ${PORT}`);
 });
